427d405ef160476524d8e5ca192cda66d088c0ea
Sigrid <sigridaasma@gmail.com>
2017-02-15 23:09:49 +0200

    lisan failid

---
+++ src/ColorSort.java
@@ -0,0 +1,16 @@
+public class ColorSort {
+
+   enum Color {red, green, blue};
+   
+   public static void main (String[] param) {
+      // for debugging
+   }
+   
+   public static void reorder (Color[] balls) {
+      // Siia tuleb minu kood
+      return;
+   }
+
+
+}
+
